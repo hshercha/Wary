78f4c41db902eba70578fc38fd139b7f63917052
Sherchan, Himal <Himal.Sherchan@garmin.com>
2016-05-24 20:11:14 -0500

    First trial. Added location and motion framework

--- Wary/Wary/ViewController.m
+++ Wary/Wary/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <CoreMotion/CoreMotion.h>
 
-@interface ViewController ()
+@interface ViewController ()<CLLocationManagerDelegate>
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CMMotionManager *motionManager;;
+@property (weak, nonatomic) IBOutlet UILabel *labelLocation;
 
 @end
 
@@ -16,6 +21,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+    self.locationManager.distanceFilter = 10;
+    
+    [self.locationManager requestAlwaysAuthorization];
+    [self.locationManager startUpdatingLocation];
+    
+    self.motionManager = [[CMMotionManager alloc] init];
+    BOOL canUseMotion = self.motionManager.deviceMotionAvailable;
+    
+    if (canUseMotion)
+    {
+        [self.motionManager startDeviceMotionUpdatesToQueue:[[NSOperationQueue alloc] init] withHandler:^(CMDeviceMotion * _Nullable motion, NSError * _Nullable error) {
+            NSLog(@"New Device datat: %@", motion);
+        }];
+    }
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +46,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
+{
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        CLLocation *location = [locations lastObject];
+        self.labelLocation.text = [NSString stringWithFormat:@"%f", location.speed];
+    });
+}
 @end
